0c8504f12b196333a4ab66a6b14c6daed5fd0299
Jonathan Olson <jonathan.olson@colorado.edu>
2018-01-26 15:01:55 -0700

    Removing scaled game references

--- js/area-model-multiplication-main.js
+++ js/area-model-multiplication-main.js
@@ -12,8 +12,6 @@ define( function( require ) {
   var ExploreScreen = require( 'AREA_MODEL_COMMON/screens/ExploreScreen' );
   var GenericGameScreen = require( 'AREA_MODEL_COMMON/screens/GenericGameScreen' );
   var GenericScreen = require( 'AREA_MODEL_COMMON/screens/GenericScreen' );
-  var ScaledGameChallengesScreen = require( 'AREA_MODEL_COMMON/screens/ScaledGameChallengesScreen' );
-  var ScaledGameScreen = require( 'AREA_MODEL_COMMON/screens/ScaledGameScreen' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var GenericGameChallengesScreen = require( 'AREA_MODEL_COMMON/screens/GenericGameChallengesScreen' );
@@ -36,10 +34,8 @@ define( function( require ) {
   SimLauncher.launch( function() {
     var sim = new Sim( areaModelMultiplicationTitleString, [
       new ExploreScreen(),
-      new ScaledGameScreen(),
       new GenericScreen(),
       new GenericGameScreen(),
-      new ScaledGameChallengesScreen(),
       new GenericGameChallengesScreen()
     ], simOptions );
     sim.start();
