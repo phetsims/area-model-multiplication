95bf38aec301f5eb1c95ee60a6ed1d61a87140e5
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:27 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/areaModelMultiplicationStrings.ts
+++ js/areaModelMultiplicationStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import areaModelMultiplication from './areaModelMultiplication.js';
 
-const areaModelMultiplicationStrings = getStringModule( 'AREA_MODEL_MULTIPLICATION' );
+type StringsType = {
+  'area-model-multiplication': {
+    'title': string
+  }
+};
+
+const areaModelMultiplicationStrings = getStringModule( 'AREA_MODEL_MULTIPLICATION' ) as StringsType;
 
 areaModelMultiplication.register( 'areaModelMultiplicationStrings', areaModelMultiplicationStrings );
 
